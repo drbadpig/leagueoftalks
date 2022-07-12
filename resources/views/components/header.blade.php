25f4251e54e691cdda7fec08f9bcff809f3d2185
drbadpig <stuartkirk14@gmail.com>
2022-07-12 23:25:29 +0300

    made header darker

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 border-b border-slate-900/10 dark:border-slate-50/[0.06] bg-black/10 supports-backdrop-blur:bg-white/95">
+    class="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 border-b border-slate-900/10 dark:border-slate-50/[0.06] bg-black/20 supports-backdrop-blur:bg-white/95">
     <div class="max-w-8xl mx-auto">
         <div class="py-4 lg:px-8 lg:border-0 mx-4 lg:mx-0">
             <div class="relative flex items-center">
