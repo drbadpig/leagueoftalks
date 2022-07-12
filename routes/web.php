81654dd99d3680943460e37dba47cc987140142a
drbadpig <stuartkirk14@gmail.com>
2022-07-12 22:47:16 +0300

    add profile route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/profile', function() {
+    return View::make('profile', ['name' => 'Samantha']);
 });
