e5ead15aa1f2cd25a6df22d2e53a5c16d91f4490
drbadpig <stuartkirk14@gmail.com>
2022-07-14 18:38:32 +0300

    add breeze & vue

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\View;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +16,16 @@ use Illuminate\Support\Facades\View;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::get('/profile', function() {
-    return View::make('profile', ['name' => 'Samantha']);
-});
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
