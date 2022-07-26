81628fcbc6189f5735554cb37924de02864dfe1b
drbadpig <stuartkirk14@gmail.com>
2022-07-26 17:45:58 +0300

    add achievement, users_ach, users_online seeders

--- database/migrations/2022_07_26_132013_create_users_onlines_table.php
+++ database/migrations/2022_07_26_132013_create_users_onlines_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->dateTime('date');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
