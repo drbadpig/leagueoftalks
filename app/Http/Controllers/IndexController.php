289334d70ba893a721b65388db4ba447da67395f
drbadpig <stuartkirk14@gmail.com>
2022-07-26 17:45:07 +0300

    add /test route

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class IndexController extends Controller
 {
-    //
+    public function test() {
+        dd(User::first()->id);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Inertia\Inertia;
 |
 */
 
+Route::get('/test', [\App\Http\Controllers\IndexController::class, 'test']);
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
